1ef91a5b9a79a5a041020e8298a0ac3ea16688f8
RealET <real3t@gmail.com>
2024-09-27 14:46:04 +0200

    fix: des chaînes de langue de SoyezCréateurs dans le privé

---
+++ lang/soyezcreateursprive_fr.php
@@ -0,0 +1,9 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+return [
+
+	'cfg_pageauteurs_label' => 'Pages des auteurs',
+	'cfg_pageauteurs_ex' => 'Par défaut, le site dispose d’une page par auteur, ainsi que la liste de toutes les auteurs. Vous pouvez décider d’en bloquer l’accès.<br /><strong>Attention</strong> : si vous masquez ces pages, pour pouvoir entrer en contact, il faudra avoir mis en place un formulaire de contact avec le plugin Formidable.',
+
+];
