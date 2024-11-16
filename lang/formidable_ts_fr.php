7b4e5f729e708a68ebce760ce165eacd27ee1be9
RealET <real3t@gmail.com>
2024-11-16 14:23:59 +0100

    fix: un petit nouveau : Formidable Table Sorter

---
+++ lang/formidable_ts_fr.php
@@ -0,0 +1,10 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/formidable_tablesorter.git
+
+return [
+
+	// A
+	'auteur' => 'Auteur'
+
+];
