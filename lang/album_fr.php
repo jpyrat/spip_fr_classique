7a453495b94a4a76f2c66a0b9bb2ad8d7df173da
RealET <real3t@gmail.com>
2025-05-09 13:26:14 +0200

    fix: Album aussi

---
+++ lang/album_fr.php
@@ -0,0 +1,8 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/albums.git
+
+return [
+
+	'message_confirmer_action' => 'Cette action ne peut être annulée, êtes-vous sûr ?',
+];
