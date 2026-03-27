115b2828e13dcd695d50b188df147519a1d74bc2
RealET <real3t@gmail.com>
2026-03-27 14:38:07 +0100

    fix: corriger aussi Formidable Participation

---
+++ lang/formidableparticipation_fr.php
@@ -0,0 +1,9 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/formidable_participation.git
+
+return [
+
+	// T
+	'traiter_participation_champ_participant' => 'Participant',
+];
